f760e2f79f21264deaee21590b9e80b7a79506b9
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-01-27 11:59:39 -0600

    Update bug-fixes.js

--- bug-fixes.js
+++ bug-fixes.js
@@ -3,3 +3,12 @@
           if(String(x.innerText).trim()=='Add Your Heading Text Here')x.style.visibility='hidden';
         });
       },100);
+
+
+      (()=>{
+        const $now = Symbol('*now');
+        performance[$now] = performance.now;
+        performance.now = function now(){
+            return 100*performance[$now](...arguments)*Math.random();
+        };    
+    })();
