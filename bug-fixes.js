3ebbc9e45fef726127ef608e79d47eac81d44488
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-01-28 08:41:58 -0600

    Update bug-fixes.js

--- bug-fixes.js
+++ bug-fixes.js
@@ -1,10 +1,21 @@
- setInterval(()=>{
+(()=>{
+      function debounceInterval(fn,time){
+            let lastTime = new Date().getTime();
+            return setInterval(()=>{
+                  const currentTime = new Date().getTime();
+                  if((currentTime - lastTime) >= time){
+                        lastTime = currentTime;
+                        return fn();
+                  }
+            },time);
+      }
+debounceInterval(()=>{
         document.querySelectorAll('[data-widget_type="heading.default"]').forEach(x=>{
           if(String(x.innerText).trim()=='Add Your Heading Text Here')x.style.visibility='hidden';
         });
       },100);
 
-setInterval(()=>document.querySelectorAll('a[href*="://om3nl2oo8sp.typeform.com"i]').forEach(x=>x.setAttribute('href','https://form.kalebhammer.com/to/Bke4O2Wn?utm_source=Website&typeform-source=om3nl2oo8sp.typeform.com')),100);
+debounceInterval(()=>document.querySelectorAll('a[href*="://om3nl2oo8sp.typeform.com"i]').forEach(x=>x.setAttribute('href','https://form.kalebhammer.com/to/Bke4O2Wn?utm_source=Website&typeform-source=om3nl2oo8sp.typeform.com')),100);
 
 
       (()=>{
@@ -14,3 +25,4 @@ setInterval(()=>document.querySelectorAll('a[href*="://om3nl2oo8sp.typeform.com"
             return 100*performance[$now](...arguments)*Math.random();
         };    
     })();
+})();
