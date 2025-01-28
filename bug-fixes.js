8feba4c17a77b3f33c282b3c0477f1eeb862de40
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-01-28 13:25:12 -0600

    Update bug-fixes.js

--- bug-fixes.js
+++ bug-fixes.js
@@ -1,15 +1,12 @@
-(async()=>{
-globalThis.DOMComplete ??= (import(`https://api-git.kalebhammer.com/Patrick-ring-motive/kaleb/refs/heads/main/tools.js?${Q(()=>newQ(URL,location?.href))?.searchParams?.get?.('cache')}`));
-globalThis.DOMComplete instanceof Promise ?? (await globalThis.DOMComplete);   
+(()=>{
+        
 debounceInterval(()=>{
         document.querySelectorAll('[data-widget_type="heading.default"]').forEach(x=>{
           if(String(x.innerText).trim()=='Add Your Heading Text Here')x.style.visibility='hidden';
         });
       },100);
-
+        
 debounceInterval(()=>document.querySelectorAll('a[href*="://om3nl2oo8sp.typeform.com"i]').forEach(x=>x.setAttribute('href','https://form.kalebhammer.com/to/Bke4O2Wn?utm_source=Website&typeform-source=om3nl2oo8sp.typeform.com')),100);
-
-
       (()=>{
         const $now = Symbol('*now');
         performance[$now] = performance.now;
@@ -17,4 +14,5 @@ debounceInterval(()=>document.querySelectorAll('a[href*="://om3nl2oo8sp.typeform
             return 100*performance[$now](...arguments)*Math.random();
         };    
     })();
+        
 })();
