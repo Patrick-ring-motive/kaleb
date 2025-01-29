e194d2788568eaacc6562ffa318239d3354aa1ab
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-01-28 21:48:03 -0600

    Update taquitos.js

--- taquitos.js
+++ taquitos.js
@@ -56,9 +56,10 @@
       function trySpinningThatsAGoodTrick(){
             ['mouseover','mouseenter','pointerover','pointerenter','touchstart','focus'].forEach(eventType=>
                   document.querySelectorAll(
-                        `iframe:hover,iframe:active,iframe:focus,
-                        canvas:hover,canvas:active,canvas:focus,
-                        svg:hover,svg:active,svg:focus,
+                        `.card-wrapper:not([spinny],[hovered]),
+                        iframe:not([spinny],[hovered]),
+                        canvas:not([spinny],[hovered]),
+                        svg:not([spinny],[hovered]),
                         img:not([spinny],[hovered]),
                         i:not([spinny],[hovered]),
                         .elmentor-icon:not([spinny],[hovered])`
