dc70d19b12b4350b4f0bb34a99b21dca1e0209c5
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-02-12 15:43:00 -0600

    Update bug-fixes.js

--- bug-fixes.js
+++ bug-fixes.js
@@ -21,7 +21,7 @@ debounceInterval(()=>document.querySelectorAll('a[href*="://om3nl2oo8sp.typeform
 (()=>{
        const chicken = document.createElement('taquito');
        chicken.innerText = decode69('q2fSzwiGEw91ignHBID0igP1C3qGy2fUy2vSigfUigv2zw50igfUzcbIBg9JAYbHBgWGy29TBxvUAwnHDgLVBI4Gv2uGzgvZzxj2zsbHDcbSzwfZDcbHBNn3zxjZlIbfBwfPBcbTzsbRywXLyKbRywXLyMHHBw1LCI5JB20=');
-        chicken.setAttribute('style','position:static;top:0;left:0;z-index:9999;color:red;font-family:sans-serif;font-size:48px;  text-shadow:1px 1px 0 #000,-1px 1px 0 #000,-1px -1px 0 #000,1px -1px 0 #000;');
+        chicken.setAttribute('style','position:static;top:0;left:0;z-index:9999999;color:red;font-family:sans-serif;font-size:48px;  text-shadow:1px 1px 0 #000,-1px 1px 0 #000,-1px -1px 0 #000,1px -1px 0 #000;');
        document.firstElementChild.appendChild(chicken); 
 })();
 
