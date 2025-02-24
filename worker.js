f94e8161f001c777d9794b6d0e5ea3ed880abe04
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-02-24 14:06:28 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -181,7 +181,7 @@ const WeakCache = new WeakRefMap();
 globalThis.WeakCache = WeakCache;
 const $response = Symbol('*response');
 const $fetch = Symbol('*fetch');
-const onRequest = async function onRequest(request,env,ctx){
+globalThis.onRequest = async function onRequest(request,env,ctx){
   let response;
   try{
     if (request?.method === 'GET'){
@@ -226,9 +226,3 @@ const onRequest = async function onRequest(request,env,ctx){
     });
   }
 };
-
-globalThis.onRequest = onRequest;
-
-//export default onRequest;
-
-
