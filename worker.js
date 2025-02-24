f01fa9f0067c4d516acf9c9598ad5768243da566
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-02-24 12:37:06 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -181,7 +181,7 @@ const isValidResponse = x => (x?.status === 200 && !x?.bodyUsed && !x?.body?.loc
 globalThis.WeakCache = new WeakRefMap();
 const $response = Symbol('*response');
 const $fetch = Symbol('*fetch');
-async function onRequest(request,env,ctx){
+const onRequest = async function onRequest(request,env,ctx){
   let response;
   try{
     if (request?.method === 'GET'){
