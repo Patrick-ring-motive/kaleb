5302624b8088079ab4fbfa0bffe046c34e414714
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-02-24 14:58:13 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -83,7 +83,7 @@ globalThis.$weakRefMap = Symbol('*weakRefMap');
 
 
 
-globalThis.isValidResponse = x => ((x?.status === 200) && !(x?.bodyUsed) && !(x?.body?.locked)) || x?.status === 304;
+globalThis.isValidResponse = x => {return ((x?.status === 200) && !(x?.bodyUsed) && !(x?.body?.locked)) || x?.status === 304};
 globalThis.WeakCache = new WeakRefMap();
 const $response = Symbol('response');
 globalThis.onRequest = async function(request,env,ctx){
