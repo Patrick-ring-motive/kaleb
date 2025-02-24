5b24978e274b00db82fe5782df994c11494975e2
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-02-24 10:56:38 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -27,8 +27,12 @@ function replaceResponseHosts(s){
   }
   return s;
 }
+
+
+
+
 const defaultHost = "calebhammer.com";
-async function onRequest(request,env,ctx) {
+globalThis.onReq = async function onReq(request,env,ctx) {
   if(/favicon/i.test(request.url))return fetch("https://kalebhammer.com/wp-content/uploads/2024/02/cropped-Financial-Audit-Transparent-Background-32x32.png");
   if(/sw\.js/i.test(request.url)){
     return new Response(
@@ -120,5 +124,106 @@ function cleanResponse(response){
   response.headers.delete('Cross-Origin-Embedder-Policy');
 return response;
 }
+
+
+(()=>{
+const instanceOf=(x,y) =>{
+  try{
+    return x instanceof y;
+  }catch{
+    return false;
+  }
+};
+const WeakRefMap = (()=>{
+  const $weakRefMap = Symbol('*weakRefMap');
+  return class WeakRefMap extends Map {
+      constructor() {
+        super();
+        this[$weakRefMap] = new Map();
+      }
+
+      get(key) {
+        const ref = this[$weakRefMap].get(key);
+        const value = ref?.deref?.();
+        if (value === undefined) {
+          this[$weakRefMap].delete(key);
+        }
+        return value;
+      }
+
+      set(key, value) {
+        this[$weakRefMap].set(key, new WeakRef(value));
+        return this;
+      }
+
+      delete(key) {
+        return this[$weakRefMap].delete(key);
+      }
+
+      has(key) {
+        const value = this[$weakRefMap].get(key)?.deref?.();
+        if (value === undefined) {
+          this[$weakRefMap].delete(key);
+          return false;
+        }
+        return true;
+      }
+    }
+  })();
+
+
+const isValidResponse = x => (x?.status === 200 && !x?.bodyUsed && !x?.body?.locked) || x?.status === 304;
+  
+globalThis.WeakCache = new WeakRefMap();
+const $response = Symbol('*response');
+const $fetch = Symbol('*fetch');
+globalThis.onRequest = async function onRequest(request,env,ctx){
+  let response;
+  try{
+    if (request?.method === 'GET'){
+      let cachedResponse = WeakCache.get(request.url);
+      if (cachedResponse) {
+        request[$response] = cachedResponse;
+        if(cachedResponse instanceof Promise){
+          cachedResponse = await cachedResponse;
+          if(isValidResponse(cachedResponse)){
+            WeakCache.set(request.url,cachedResponse);
+          }else{
+            WeakCache.delete(request.url);
+          }
+        }
+        try{
+          response = cachedResponse.clone();
+          response[$response] = cachedResponse;
+        }catch{
+          WeakCache.delete(request.url);
+        }
+        console.log('response from cache');
+      } else {
+        const presponse = globalThis.onReq(...arguments);
+        WeakCache.set(request.url,presponse);
+        response = await presponse;
+        if (response.status === 200 && !response.bodyUsed) {
+          WeakCache.set(request.url, response.clone());
+        }else{
+          WeakCache.delete(request.url);
+        }
+      }
+    }
+    if(!instanceOf(response,Response)){
+     response = await globalThis.onReq(...arguments);
+    }
+    return response;
+  }catch(e){
+    WeakCache.delete(request.url);
+    return new Response(Object.getOwnPropertyNames(e).map(x=>`${x} : ${e[x]}`).join(''),{
+      status : 569,
+      statusText:e.message
+    });
+  }
+};
+
+})();
+
 
 export default onRequest;
